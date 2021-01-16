53d17d18c22e263624bf59268c791c364c48f077
unknown <gelo.tabada.10@gmail.com>
2021-01-16 16:18:46 +0800

    commit

--- mainProg.py
+++ mainProg.py
@@ -464,21 +464,18 @@ class Ui_MainWindow(object):
                     mydb_conn.commit()
             attendance=attendance.drop_duplicates(subset=['Id'],keep='first')
             cv2.imshow('im',im)
-            if (cv2.waitKey(1)==ord('q')):
+            if cv2.waitKey(100) & 0xFF == ord('x'):
                 mycursor.close()
-                cam.release()
-                cv2.destroyAllWindows()
                 break
         #save attendance of detected faces into csv file
+        cam.release()
+        cv2.destroyAllWindows()
         ts = time.time()      
         date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
         timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
         Hour,Minute,Second=timeStamp.split(":")
         fileName="Attendance\Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
         attendance.to_csv(fileName,index=False)
-        #print(attendance)
-        res=attendance 
-        self.lineEdit_notif.setText(res)
 
 if __name__ == "__main__":
     import sys
